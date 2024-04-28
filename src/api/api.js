3d507977233dbd389bc00326f6a99ec80711e765
Divya Prakash Singh <divyaprakashs169@gmail.com>
2024-04-29 04:49:41 +0530

    weather App using react js and open weather api

---
+++ src/api/api.js
@@ -0,0 +1,11 @@
+const apiKey = '8245357fcd6026288d24f8bf50217ad3';
+
+const getWeather = async (city) => {
+    return await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
+    .then((res) => res.json())
+    .then((json) => {
+        return json;
+    })
+}
+
+export default getWeather;
